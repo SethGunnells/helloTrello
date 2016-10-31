60dadbb5f6642643dc7b13579ea492585668391a
Seth Gunnells <sethgunnells@gmail.com>
2016-10-31 00:50:23 -0500

    First stab at a Trello-like appearance with static data

--- src/HelloTrelloApp.jsx
+++ src/HelloTrelloApp.jsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
+import {List} from './List.jsx';
+import '../styles/index.css';
+
+const cardData = [
+  {id: 1, text: 'hello'},
+  {id: 2, text: 'world'},
+  {id: 3, text: 'i am a card'}
+];
 
 export class HelloTrelloApp extends React.Component {
   render() {
-    return <h1>Hello, Trello!</h1>;
+    return <List cards={cardData} />;
   }
 }
